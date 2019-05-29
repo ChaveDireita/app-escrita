9d0f187b3c5b6c4fa52c8e94d7585684ee6f6904
ChaveDireita <geometrianaoeuclidiana@gmail.com>
2019-05-29 08:15:38 -0300

    Terminado a tela de login. Só faltao título.

--- app/src/main/java/br/alunos/appescrita/activities/ActivityLogin.java
+++ app/src/main/java/br/alunos/appescrita/activities/ActivityLogin.java
@@ -1,13 +1,48 @@
 package br.alunos.appescrita.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class ActivityLogin extends AppCompatActivity {
+import br.alunos.appescrita.R;
+
+public class ActivityLogin extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+
+        EditText editText = findViewById(R.id.campo_senha);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener()
+        {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
+            {
+                if (actionId == EditorInfo.IME_ACTION_DONE)
+                {
+                    Log.d("Debug", "oeal");
+                    //iniciarActivity();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void iniciarActivity (Class activityClass)
+    {
+        Intent intent = new Intent(this, activityClass);
+        startActivity (intent);
+        finish();
     }
 }
--- app/src/main/java/br/alunos/appescrita/activities/SplashScreen.java
+++ app/src/main/java/br/alunos/appescrita/activities/SplashScreen.java
@@ -1,5 +1,6 @@
 package br.alunos.appescrita.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import br.alunos.appescrita.R;
@@ -12,7 +13,16 @@ public class SplashScreen extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    }
 
 
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Intent intent = new Intent(this, ActivityLogin.class);
+        startActivity(intent);
+
+        finish();
     }
 }
