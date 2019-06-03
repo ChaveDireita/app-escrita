5089b0c322dcf36f20db309942176431003daaec
ChaveDireita <geometrianaoeuclidiana@gmail.com>
2019-06-03 00:47:22 -0300

    Acabou! Finalmente!

--- app/src/main/java/br/alunos/appescrita/activities/SplashScreen.java
+++ app/src/main/java/br/alunos/appescrita/activities/SplashScreen.java
@@ -4,32 +4,27 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import br.alunos.appescrita.R;
 
 public class SplashScreen extends AppCompatActivity implements Runnable
 {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
         getSupportActionBar().hide();
 
         new Handler().postDelayed(this, 3000);
+
     }
 
     @Override
     public void run()
     {
-        iniciarActivity(ActivityLogin.class);
-    }
-
-    public void iniciarActivity (Class activityClass)
-    {
-        Intent intent = new Intent(this, activityClass);
-        startActivity (intent);
+        Intent abrirtelalogin = new Intent(this, ActivityLogin.class);
+        startActivity(abrirtelalogin);
         finish();
     }
 }
